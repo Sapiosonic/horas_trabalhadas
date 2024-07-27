4f4f9e8424188d841661c003bba4c39ea8b4422f
Radamés <radames.idiomas@gmail.com>
2024-07-27 00:13:56 -0300

    feat (users::model): Adiciona mensagens padrão do sistema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  validates_presence_of :login, :password, :name, :email
-  validates_length_of :login, :in => 5..100
-  validates_length_of :password, :in => 5..50
-  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/
-  validates_uniqueness_of :login
-  validates_uniqueness_of :email
+  validates_presence_of :login, :password, :name, :email, message: "não pode ser vázio"
+  validates_length_of :login, :in => 5..50, message: "deve ter entre 5 e 50 caracteres!"
+  validates_length_of :password, :in => 5..50, message: "deve ter entre 5 e 50 caracteres!"
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/, message: "inválido!"
+  validates_uniqueness_of :login, message: "indisponível."
+  validates_uniqueness_of :email, message: "indisponível."
 end
